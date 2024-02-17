e8784a735ed4725d3aa230ad13434b1bd2fd4f2f
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-02-17 18:52:10 +0530

    map code update

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+  },
+  function () {
+    alert('Geolocation is not available');
+  }
+);
