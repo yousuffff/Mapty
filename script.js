2b52b51595c7d7536b1cedc2e7bc78abcef74e24
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-02-19 19:00:07 +0530

    update

--- script.js
+++ script.js
@@ -13,11 +13,22 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation.getCurrentPosition(
   function (position) {
-    const { latitude, longitude } = position.coords
+    const { latitude, longitude } = position.coords;
     console.log(latitude, longitude);
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
   },
   function () {
     alert('Geolocation is not available');
   }
 );
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([51.5, -0.09])
+  .addTo(map)
+  .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+  .openPopup();
