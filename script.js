8636dcd7e9c2e9a81faab4d340dd3a8c9df08bdb
Mohd Yousuf <77265813+yousuffff@users.noreply.github.com>
2024-12-05 17:33:46 +0530

    Update script.js
    Change in Leaflet API

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ class App {
 
     this.#map = L.map.bind(this)('map').setView(coord, 13);
 
-    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
